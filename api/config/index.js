15f2307f006a566da26ae4b79260fb4deb59a4fe
silagul0108 <silagul22@gmail.com>
2025-06-16 23:13:47 +0300

    #10

--- api/config/index.js
+++ api/config/index.js
@@ -1,7 +1,7 @@
 module.exports={
     "PORT": process.env.PORT || "3000",
     "LOG_LEVEL": process.env.LOG_LEVEL || "debug",
-    "CONNECTION_STRING": process.env.CONNECTION_STRING || "mongodb+srv://roka:JsoMR1z8VhIxKhGO@cluster.rairmof.mongodb.net/?retryWrites=true&w=majority&appName=Cluster",
+    "CONNECTION_STRING": "mongodb+srv://roka:JsoMR1z8VhIxKhGO@cluster.rairmof.mongodb.net/?retryWrites=true&w=majority&appName=Cluster",
     "JWT": {
         "SECRET":  process.env.JWT_SECRET || "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9eyJpZCI6IjY0NjY5OGUwZTUzYmI4MWVhYzlmYTc4NSIsImV4cCI6MTQ5NDg5NDk2NDMyMDAwfQv0SJjaBuR7SrLmvhVWxAiKgz4T4_rX55wHlD9CfBW10lmYTc4NSIsImV4cCI6MTQ5NDg5NDk2NDMyiLCJhbGciOiJI",
         "EXPIRE_TIME": !isNaN(parseInt(process.env.TOKEN_EXPIRE_TIME)) ? parseInt(process.env.TOKEN_EXPIRE_TIME) : 24 * 60 * 60 
