1bb50a3b854f089cf5742fbda1c5dedcedfaa60b
silagul0108 <silagul22@gmail.com>
2025-06-17 00:25:06 +0300

    #10

---
+++ api/config/index.js
@@ -0,0 +1,12 @@
+module.exports={
+    "PORT": process.env.PORT || "3000",
+    "LOG_LEVEL": process.env.LOG_LEVEL || "debug",
+    "CONNECTION_STRING": "mongodb+srv://roka:JsoMR1z8VhIxKhGO@cluster.rairmof.mongodb.net/roka_restaurant?retryWrites=true&w=majority",
+    "JWT": {
+        "SECRET":  process.env.JWT_SECRET || "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9eyJpZCI6IjY0NjY5OGUwZTUzYmI4MWVhYzlmYTc4NSIsImV4cCI6MTQ5NDg5NDk2NDMyMDAwfQv0SJjaBuR7SrLmvhVWxAiKgz4T4_rX55wHlD9CfBW10lmYTc4NSIsImV4cCI6MTQ5NDg5NDk2NDMyiLCJhbGciOiJI",
+        "EXPIRE_TIME": !isNaN(parseInt(process.env.TOKEN_EXPIRE_TIME)) ? parseInt(process.env.TOKEN_EXPIRE_TIME) : 24 * 60 * 60 
+    },
+    "DEFAULT_LANG": process.env.DEFAULT_LANG || "EN",
+    "FILE_UPLOAD_PATH": process.env.FILE_UPLOAD_PATH,
+    "MANAGER_ROLE_ID": "684b5260c6819a4670387f95" || ""
+}
---
+++ api/db/Database.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+let instance = null;
+
+class Database {
+
+    constructor() {
+        if(!instance) {
+            this.mongoConnection = null;
+            instance = this;
+        }
+        return instance;
+    }
+
+    async connect(options) {
+        try {
+            console.log("DB is connecting");
+            mongoose.connection.once("open", () => {
+              console.log("✅ Mongoose bağlandı:", mongoose.connection.name);
+            });
+            
+            this.mongoConnection = await mongoose.connect(options.CONNECTION_STRING );
+
+            
+            console.log("DB is connected");
+
+        }catch(err){
+            console.error(err);
+            process.exit(1);
+        }
+    }
+
+}
+
+module.exports = Database
